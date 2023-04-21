b80385c52f9109a2c4e4cd2ee69d9b6ba769013c
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-22 00:49:25 +0530

    rest card updated

--- src/components/RestaurantCard/RestaurantCard.jsx
+++ src/components/RestaurantCard/RestaurantCard.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 import './restaurantcard.css';
 
-function RestaurantCard({ restaurantName }) {
+function RestaurantCard({ restaurantName, rating }) {
     return (
         <div className="restaurant-card">
             <Link to={`/restaurants/${restaurantName}`} state={{ name: restaurantName }}>
                 <h1>{restaurantName}</h1>
+                <div className='restaurant-card-info'>
+                    <p>Rating: {rating}</p>
+                    <p>Type: </p>
+                </div>
             </Link>
 
         </div>
