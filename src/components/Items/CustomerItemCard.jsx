3b30ff8a9d6ad1aef50d5f945e671022383d8853
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-22 00:01:11 +0530

    CusotmerItemCard

---
+++ src/components/Items/CustomerItemCard.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./CustomerItemCard.css";
+
+const CustomerItemCard = ({ name, imageUrl, description, price }) => {
+    return (
+        <div className="item-card">
+            <img className="item-card-image"
+                src={imageUrl}
+                alt={name}
+            ></img>
+
+            <div className="item-card-content">
+                <p className="item-card-name">{name}</p>
+                <p className="item-card-description">{description}</p>
+                <p className="item-card-price">${price}</p>
+                <button className="item-card-button">
+                    Add To Cart
+                </button>
+            </div>
+        </div >
+    );
+};
+
+export default CustomerItemCard;
