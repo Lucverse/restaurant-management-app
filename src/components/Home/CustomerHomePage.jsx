36fe6c2defcac6ca79b6f62e6bef65c4e25953ab
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-21 21:31:45 +0530

    cusotm home page

--- src/components/Home/CustomerHomePage.jsx
+++ src/components/Home/CustomerHomePage.jsx
@@ -1,7 +1,28 @@
-function CustomerHomePage(){
-    return(
+import { API_URL } from "../../types/types";
+import { useEffect, useState } from "react";
+import RestaurantCard from "../RestaurantCard/RestaurantCard";
+function CustomerHomePage() {
+    const [restaurant, setRestaurant] = useState([]);
+    useEffect(() => {
+        const fetchItemsData = async () => {
+            try {
+                const response = await fetch(`${API_URL}/restaurants`);
+                const data = await response.json();
+                setRestaurant(data);
+            } catch (error) {
+                console.error("Error fetching items data: ", error);
+            }
+        };
+        fetchItemsData();
+    }, []);
+    return (
         <div>
-            <h1>Customer Home Page</h1>
+            <h1>Restaurants For You</h1>
+            {restaurant.map((item) => (
+                <RestaurantCard
+                    key={item.id}
+                    restaurantName={item.restaurantName} />
+            ))}
         </div>
     )
 }
