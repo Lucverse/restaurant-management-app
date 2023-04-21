261b1c59be8998374f333553f515ef877e3c974b
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-21 21:31:28 +0530

    staff hoem page updated

--- src/components/Home/StaffHomePage.jsx
+++ src/components/Home/StaffHomePage.jsx
@@ -1,76 +1,57 @@
 import React, { useState, useEffect } from "react";
-// import { useSelector } from "react-redux";
 import AddItem from "../AddItem/AddItem";
 import ItemCard from "../Items/ItemCard";
 import './staffhome.css'
+import { API_URL } from "../../types/types";
+import { useSelector } from "react-redux";
 
 function StaffHomePage() {
     const [showAddItem, setShowAddItem] = useState(false);
     const [items, setItems] = useState([]);
-    // const [tobeedited, settobeedited] = useState([]);
-    // const username = useSelector(state => state.auth.user?.username || null);
+    const restaurantName = useSelector(state => state.auth.user.restaurantName);
 
     useEffect(() => {
         const fetchItemsData = async () => {
             try {
-                const response = await fetch("http://localhost:5000/items");
+                const response = await fetch(`${API_URL}/items`);
                 const data = await response.json();
-                console.log(data);
-                const filteredData = data.filter(item => item.restaurant === "Restaurant 1");
+                const filteredData = data.filter(item => item.restaurantName === restaurantName);
                 setItems(filteredData);
             } catch (error) {
                 console.error("Error fetching items data: ", error);
             }
         };
         fetchItemsData();
-    }, []);
-    
+    }, [restaurantName]);
+
 
     const handleAddItemClick = () => {
         setShowAddItem(true);
     };
-
+    const handleCancel = () => {
+        setShowAddItem(false);
+    };
     const handleEditClick = async (id, name, description, price) => {
-        try {
-            const response = await fetch(`http://localhost:5000/items/:${id}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ name, description, price }),
-            });
-            const updatedItem = await response.json();
-            setItems((prevItems) =>
-                prevItems.map((item) => {
-                    if (item.id === updatedItem.id) {
-                        return updatedItem;
-                    } else {
-                        return item;
-                    }
-                })
-            );
-        } catch (error) {
-            console.error("Error updating item data: ", error);
-        }
+        // console.log(id,name);
     };
 
     return (
         <div>
             <h1>Staff Home Page</h1>
+            <button onClick={handleAddItemClick}>Add a new item</button>
             <div className="item-card-container">
                 {items.map((item) => (
                     <ItemCard
                         key={item.id}
-                        name={item.name}
-                        imageUrl={item.image}
-                        price={item.price}
-                        description={item.description}
+                        name={item.itemName}
+                        imageUrl={item.itemImage}
+                        price={item.itemPrice}
+                        description={item.itemDescription}
                         onEditClick={(name, description, price) => handleEditClick(item.id, name, description, price)}
                     />
                 ))}
             </div>
-            <button onClick={handleAddItemClick}>Add a new item</button>
-            {showAddItem && <AddItem />}
+            {showAddItem && <AddItem onCancel={handleCancel} />}
         </div>
     );
 }
