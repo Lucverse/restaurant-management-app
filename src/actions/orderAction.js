7dc15aff1c221d72031f50fa96abdca5fbbfc3c9
Luc <Luc1fer.tms.ig@gmail.com>
2023-04-27 11:35:14 +0530

    fetching and storing orders in redux store

---
+++ src/actions/orderAction.js
@@ -0,0 +1,46 @@
+import { API_URL, FETCH_ORDERS_REQUEST, FETCH_ORDERS_SUCCESS, FETCH_ORDERS_FAILURE } from '../types/types';
+
+export const fetchOrdersRequest = () => {
+  return {
+    type: FETCH_ORDERS_REQUEST
+  };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: FETCH_ORDERS_SUCCESS,
+    payload: orders
+  };
+};
+
+export const fetchOrdersFailure = (error) => {
+  return {
+    type: FETCH_ORDERS_FAILURE,
+    payload: error
+  };
+};
+
+export const fetchOrders = () => {
+  return (dispatch) => {
+    dispatch(fetchOrdersRequest());
+    try {
+      fetch(`${API_URL}/orders`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          dispatch(fetchOrdersSuccess(data));
+        })
+        .catch(error => {
+          console.error("Error fetching orders data: ", error);
+          dispatch(fetchOrdersFailure(error.message));
+        });
+    } catch (error) {
+      console.error("Error fetching orders data: ", error);
+      dispatch(fetchOrdersFailure(error.message));
+    }
+  };
+};
--- src/store/store.js
+++ src/store/store.js
@@ -4,12 +4,14 @@ import authReducer from '../reducers/authReducer';
 import cartReducer from '../reducers/cartReducer';
 import itemReducer from '../reducers/itemReducer';
 import restaurantReducer from '../reducers/restaurantReducer';
+import ordersReducer from '../reducers/orderReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   cart: cartReducer,
   item: itemReducer,
   restaurant: restaurantReducer,
+  order: ordersReducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk)); 
