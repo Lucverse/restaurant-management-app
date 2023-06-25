db336c9b5f68702881c727b9016a3594f058e9cc
Luc <Luc1fer.tms.ig@gmail.com>
2023-06-25 22:03:57 +0530

    noitems page

---
+++ src/components/No Items/NoItems.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import emptyCartImage from '../../media/empty cart.png';
+
+function NoItem() {
+    return (
+        <div className='emptycartdiv'>
+            <img src={emptyCartImage} alt="Empty Cart" className='emptycartimage' />
+            <h1>Add Items to your cart</h1>
+            <NavLink to="/">Home</NavLink>
+        </div>
+    );
+}
+
+export default NoItem;
