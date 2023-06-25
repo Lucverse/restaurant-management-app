a21836ccdfb228fcc13b5790700e3a04b633fb52
Luc <Luc1fer.tms.ig@gmail.com>
2023-06-26 03:27:31 +0530

    loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -12,7 +12,7 @@ function Loading() {
                 <div></div>
                 <div></div>
             </div>
-            <h2 style={{ color: 'black', textAlign:'center' }}>Loading</h2>
+            <h2 style={{ color: '#5C9EAD', textAlign:'center' }}>Loading</h2>
         </div>
     )
 }
