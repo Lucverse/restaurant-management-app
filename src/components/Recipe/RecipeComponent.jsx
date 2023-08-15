360211397e7353d407d6aaae24d3c9269373e0da
Luc <Luc1fer.tms.ig@gmail.com>
2023-08-15 22:00:45 +0530

    recipe comp responisveness

--- src/components/Recipe/RecipeComponent.jsx
+++ src/components/Recipe/RecipeComponent.jsx
@@ -5,8 +5,8 @@ function RecipeComponent() {
         <div className="recipe-main">
             <img src={image1} alt='recipe' />
             <div className='recipe-content'>
-                <h1>Subscribe to good food every week</h1>
-                <p>The easiest way to enjoy chef-prepared meals every week is to subscribe. Customize your menu as often as you’d like, or set your meal plan on autopilot. We prep, cook, and deliver so you can skip straight to enjoying.</p>
+                <h1>Subscribe to recipes every week</h1>
+                <p>Elevate your culinary journey with a weekly recipe subscription, delivering the cherished flavors of your favorite restaurants to your home. Delight in restaurant-quality meals, conveniently crafted just for you.</p>
                 <button className='learn-more'>Learn More</button>
             </div>
         </div>
